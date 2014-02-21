98a57214f568d67a412ef9c60a51748bca4eadfc
rbrlpz <jrlopez233@gmail.com>
2014-02-21 15:21:16 -0600

    Fetch de las colecciones

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
 	console.log('main.js loaded');
+
+	window.collections.articles = new Puls4.Collections.Articles;
+
+	window.collections.articles.on("add",function(model){
+		//console.log("Se ha agregado un nuevo modelo", model.toJSON());
+		//aqui debería comenzar a agregar los datos al body
+		var view = new Puls4.Views.Article({model:model});
+		
+		view.render();
+		view.$el.appendTo(".posts");
+
+	});
+
+	window.collections.articles.fetch();
 });
